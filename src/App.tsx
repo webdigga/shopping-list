d01625d2f79d15e3bcb98e46cd718640570909b5
David White <webdigga42@gmail.com>
2022-05-27 16:19:14 +0100

    Re-adding with TS + adding simple list of TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-// TODO
 
 // API Gateway - https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/
 // PUT/POST - "Content-Type: application/json" -d "{\"id\": \"abcdef234\", \"price\": 12345, \"name\": \"myitem\"}" https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/items
@@ -9,13 +8,23 @@ import './App.css';
 // GET item - https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/items/1
 // DELETE item - https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/items/1
 
+// TODO - list all items
+// TODO - Add new item
+// TODO - Edit item
+// TODO - Delete item
+// TODO - Mark item as ticked
+// TODO - Ability to untick all items
+// TODO - Cypress integration tests
+// TODO - Unit tests
+// TODO - Deploy App
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
