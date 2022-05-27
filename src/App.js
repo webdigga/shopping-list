2594026bd6cba1e45aec66ee771aa9d5864c851b
David White <webdigga42@gmail.com>
2022-05-27 16:07:45 +0100

    Adding AWS Lambda API Gateway endpoints for later use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+// TODO
+
+// API Gateway - https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/
+// PUT/POST - "Content-Type: application/json" -d "{\"id\": \"abcdef234\", \"price\": 12345, \"name\": \"myitem\"}" https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/items
+// GET all items - https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/items
+// GET item - https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/items/1
+// DELETE item - https://ary9mw0hd0.execute-api.eu-west-2.amazonaws.com/items/1
+
 function App() {
   return (
     <div className="App">
